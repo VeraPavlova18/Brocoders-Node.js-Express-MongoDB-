555b5767f7966f4bcab657af80923abc89deb52d
Vera Pavlova <pavlova.vera18@gmail.com>
2019-04-16 20:10:17 +0300

    Configure mongoose, mongoose-pagination

--- app.js
+++ app.js
@@ -1,70 +1,89 @@
 /* eslint-disable no-console */
+require('dotenv').config();
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
 const express = require('express');
 const expressHbs = require('express-handlebars');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const hbs = require('hbs');
 const moment = require('moment');
-const { MongoClient } = require('mongodb');
 
-const mongoClient = new MongoClient('mongodb://localhost:27017/', { useNewUrlParser: true });
-let dbClient;
+const Schema = mongoose.Schema;
 
-const port = process.env.port || 3000;
 const app = express();
+const reqTrapScheme = new Schema({
+  trapId: String,
+  date: String,
+  ip: String,
+  method: String,
+  protocol: String,
+  query: { type: String, default: '' },
+}, { versionKey: false });
+reqTrapScheme.plugin(mongoosePaginate);
+const Request = mongoose.model('Request', reqTrapScheme);
+
 
 app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'layout', extname: 'hbs' }));
 app.set('view engine', 'hbs');
 hbs.registerPartials(`${__dirname}/views/partials`);
 
-mongoClient.connect((err, client) => {
-  if (err) console.log(err);
-  dbClient = client;
-  app.locals.collection = client.db('requestsTrap').collection('requests');
-  app.listen(port, () => console.log('Server has started'));
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () => {
+  try {
+    app.listen(process.env.PORT, () => console.log('Server has started'));
+  } catch (err) {
+    console.err(err);
+  }
 });
 
 app.get('/', (req, res) => {
-  res.render('home', { title: 'Requests Trap' });
+  try {
+    res.render('home', { title: 'Requests Trap' });
+  } catch (err) {
+    console.err(err);
+  }
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/', (req, res) => res.redirect(`${req.body.reqExample || 'urlDefault'}`));
+app.post('/', ({ body: { reqExample } }, res) => res.redirect(reqExample || 'urlDefault'));
 
-app.get('/:trap_id/requests', (req, res) => {
-  const collection = req.app.locals.collection;
-  collection.find({}).toArray((err, requests) => {
-    if (err) console.log(err);
-    res.render('requestsList', {
-      requests,
-      title: 'Requests List',
+app.get('/:trap_id/requests', ({ query: { page } }, res) => {
+  page = +page || 1;
+
+  Request.paginate({}, { limit: 10, page })
+    .then(({ docs, total, limit }) => {
+      res.render('requestsList', {
+        requests: docs,
+        title: 'Requests List',
+        nextPage: page === Math.ceil(total / limit) ? null : page + 1,
+        prevPage: page < 1 ? null : page - 1,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  });
 });
 
-app.use('/:trap_id', (req, res, next) => {
-  const data = moment().format('MMMM Do YYYY, h:mm:ss a');
-  const reqInfo = `${data} ${req.method} ${req.url}`;
-  const request = { request: req.params.trap_id, reqInfo };
-
-  const collection = req.app.locals.collection;
-  if (request.request !== 'styles') {
-    collection.insertOne(request, (err, res) => {
-      if (err) console.log(err);
-    });
+app.use('/:trap_id', ({
+  method, ip, protocol, params: { trap_id: trapId }, query,
+}, res, next) => {
+  const date = moment().format('MMMM Do YYYY, h:mm:ss a');
+  query = JSON.stringify(query);
+  const request = new Request({
+    trapId, date, ip, method, protocol, query,
+  });
+  try {
+    request.save();
+  } catch (err) {
+    console.err(err);
   }
   next();
 });
 
-app.all('/:trap_id', (req, res) => {
-  const trapId = req.params.trap_id;
-  res.render('trapId', {
-    trapId,
-    title: 'Request trap',
-  });
-});
-
-process.on('SIGINT', () => {
-  dbClient.close();
-  process.exit();
+app.all('/:trap_id', ({ params: { trap_id: trapId } }, res) => {
+  try {
+    res.render('trapId', { trapId, title: 'Request trap' });
+  } catch (err) {
+    console.err(err);
+  }
 });
